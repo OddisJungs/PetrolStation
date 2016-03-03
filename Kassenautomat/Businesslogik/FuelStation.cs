8e613a2a6f7344c90784ac2b7511a01b9af466f6
Reto411 <buzergissera@eggrain.ch>
2016-03-03 21:23:36 +0100

    Initial Commit

---
+++ Kassenautomat/Businesslogik/FuelStation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetrolStation.Businesslogic
+{
+    public class FuelStation
+    {
+        // Konstruktor
+        public FuelStation()
+        {
+
+        }
+
+        // Private Members
+        private List<PayStation> payStations;
+        private List<PetrolPump> petrolPumps;
+        private List<PetrolTank> petrolTanks;
+
+        // Public Methoden
+        public void AlertIfFuelLevelIsCritical()
+        {
+
+        }
+
+        public void CalcSales()
+        {
+
+        }
+
+        public int GetUsedMillilitersOfMonth(int pmonth, int pfuel)
+        {
+            return 0;
+        }
+
+        public void UnlockPetrolPump(int ppetrolPumpId)
+        {
+
+        }
+
+    }
+}
