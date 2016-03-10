403086e8f201420e399c76f26d44b21c96bdbddd
C3D1 <cedric_schnider@outlook.com>
2016-03-10 14:19:09 +0100

    Test Cédric

--- PetrolStation/Businesslogik/PayStation.cs
+++ PetrolStation/Businesslogik/PayStation.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace PetrolStation.Businesslogic
 {
+    /// <summary>
+    /// Hauptklasse Paystation
+    /// </summary>
     public class PayStation
     {
         //Konstruktor
