467929c6dd7d7c89a447500d7f45260a40e84359
Reto411 <buergissera@eggrain.ch>
2016-03-17 14:23:03 +0100

    Create Singelton-PetrolPump Controller Skeleton
      + modified gitignore

---
+++ PetrolStation/Petrolstation/Petrolstation.Businesslogic/PetrolPumpController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petrolstation.Businesslogic
+{
+    public class PetrolPumpController
+    {
+        // Private Konstruktor
+        private PetrolPumpController() { }
+
+        // private members
+        private static PetrolPumpController instance;
+
+        public PetrolPumpController GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new PetrolPumpController();
+            }
+            return instance;
+        }
+
+    }
+}
