e67af9859f7dabee30d9fe64934015b51e52f3b7
C3D1 <cedric_schnider@outlook.com>
2016-03-17 14:37:45 +0100

    Add FuelType and FuelTank
    Added the two new Classes

---
+++ PetrolStation/Petrolstation/Petrolstation.Businesslogic/FuelTank.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petrolstation.Businesslogic
+{
+    public class FuelTank
+    {
+        // Private attributes
+        private int maxLevel;
+        private int fuelLevel;
+        private decimal lowCriticalVolumePercent;
+        private decimal highCriticalVolumePercent;
+        private Fueltype fuelType;
+
+        public FuelTank(int pmaxLevel, decimal plowCriticalVolumePercent, decimal phighCriticalVolumePercent, Fueltype pfuelType)
+        {
+            maxLevel = pmaxLevel;
+            fuelLevel = 0;
+            lowCriticalVolumePercent = plowCriticalVolumePercent;
+            highCriticalVolumePercent = phighCriticalVolumePercent;
+            fuelType = pfuelType;
+        }
+
+
+        public void DecreaseFuelLevel(int pamount)
+        {
+            fuelLevel = fuelLevel - pamount;
+        }
+
+    }
+}
---
+++ PetrolStation/Petrolstation/Petrolstation.Businesslogic/Fueltype.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petrolstation.Businesslogic
+{
+    public class Fueltype
+    {
+        // Private attributes
+        private string name; //Name of the Fueltype object
+        private int pricePerLiter;
+
+        // Constructor
+        public Fueltype(string pname, int ppricePerLiter)
+        {
+            name = pname;
+            pricePerLiter = ppricePerLiter;
+        }
+    }
+}
