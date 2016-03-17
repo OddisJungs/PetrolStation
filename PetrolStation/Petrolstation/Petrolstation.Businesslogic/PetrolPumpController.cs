fc3c6171407fcf3805a5486534fcd6e82936169e
C3D1 <cedric_schnider@outlook.com>
2016-03-17 14:51:47 +0100

    Merge remote-tracking branch 'origin/rewriting-everything' into rewriting-everything
    # Conflicts:
    
    #	PetrolStation/Petrolstation/Petrolstation.Businesslogic/Petrolstation.Businesslogic.csproj

---
+++ PetrolStation/Petrolstation/Petrolstation.Businesslogic/PetrolPumpController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petrolstation.Businesslogic
+{
+    public class PetrolPumpController
+    {
+        // Private Konstruktor
+        private PetrolPumpController() { }
+
+        // private members
+        private static PetrolPumpController instance;
+
+        private Dictionary<int, PetrolPump> petrolpumps;
+
+        // public methods
+        public PetrolPumpController GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new PetrolPumpController();
+            }
+            return instance;
+        }
+
+        public void UnlockPump(int ppumpId)
+        {
+            petrolpumps.Where(x => x.Id == )
+        }
+
+        public void AddPump(PetrolPump ppetrolPump)
+        {
+            int lastKey = petrolpumps.Keys.Last();
+            lastKey += 1;
+            petrolpumps.Add(lastKey, ppetrolPump);
+        }
+
+    }
+}
