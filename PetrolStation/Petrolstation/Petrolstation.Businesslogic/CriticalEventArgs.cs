1721a22c2ef41bf97ae6769d9174583bd50e2567
C3D1 <cedric_schnider@outlook.com>
2016-03-27 15:00:10 +0200

    Added Event Class and did some stuff for the event in the FuelTank Class

---
+++ PetrolStation/Petrolstation/Petrolstation.Businesslogic/CriticalEventArgs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petrolstation.Businesslogic
+{
+    public class CriticalEventArgs : EventArgs
+    {
+        private int fuelLevel;
+        private decimal fuelLevelPercent;
+        private bool snoozeOn = true;
+
+        public CriticalEventArgs(int pfuelLevel, decimal pfuelLevelPercent)
+        {
+            fuelLevel = pfuelLevel;
+            fuelLevelPercent = pfuelLevelPercent;
+        }
+
+        public int GetCriticalFuelLevel()
+        {
+            return fuelLevel;
+        }
+
+        public decimal GetCriticalFuelLevelPercent()
+        {
+            return fuelLevelPercent;
+        }
+    }
+}
