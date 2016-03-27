ae38779ca206e60ec7a0ecaa3396cb3e45950fb2
Reto Bürgisser <buergissera@hotmail.com>
2016-03-27 14:13:40 +0100

    Created Load method

--- PetrolStation/Petrolstation/Petrolstation.Businesslogic/DataContainer.cs
+++ PetrolStation/Petrolstation/Petrolstation.Businesslogic/DataContainer.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +10,36 @@ namespace Petrolstation.Businesslogic
 {
     public class DataContainer
     {
+        // private member
+        private string path;
+
+        // public Constructor
+        public DataContainer()
+        {
+            path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "\\PetrolStations\\Saves\\");
+        }
+
+        /// <summary>
+        /// Loads all object of the type T
+        /// </summary>
+        /// <typeparam name="T">Type of all objects to load</typeparam>
+        /// <returns>List of object with Type T</returns>
+        public List<T> Load<T>()
+        {
+            List<T> list = new List<T>();
+            string filespath = Path.Combine(path ,typeof(T).Name);
+            foreach (string file in Directory.GetFiles(filespath))
+            {
+                using (FileStream fileStream = File.Open(Path.Combine(filespath, file), FileMode.Open))
+                {
+                    BinaryFormatter binaryFormatter = new BinaryFormatter();
+                    list.Add((T)binaryFormatter.Deserialize(fileStream));
+                }
+            }
+
+            return list;
+        }
+
 
     }
 }
