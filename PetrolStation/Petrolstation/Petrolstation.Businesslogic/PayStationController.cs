de9266e2f1718dde4d84e545e8ed21cdb2bc87dc
Reto Bürgisser <buergissera@hotmail.com>
2016-03-27 14:48:33 +0100

    Created the structure for the PayStationController

---
+++ PetrolStation/Petrolstation/Petrolstation.Businesslogic/PayStationController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petrolstation.Businesslogic
+{
+    public class PayStationController
+    {
+        // private members
+        private static PayStationController instance;
+
+        private List<PayStation> payStations;
+
+        // Private Konstruktor
+        private PayStationController()
+        {
+            payStations = new List<PayStation>();
+        }
+
+        // public methods
+        public static PayStationController GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new PayStationController();
+            }
+            return instance;
+        }
+
+    }
+}
