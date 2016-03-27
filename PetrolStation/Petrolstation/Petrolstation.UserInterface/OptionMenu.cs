1261baf4f0a230a267607ad69cd61751195f94af
Reto Bürgisser <buergissera@hotmail.com>
2016-03-27 17:55:26 +0100

    Work forward at the petrol station

---
+++ PetrolStation/Petrolstation/Petrolstation.UserInterface/OptionMenu.cs
@@ -0,0 +1,69 @@
+﻿using Petrolstation.Businesslogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petrolstation.UserInterface
+{
+    internal class OptionMenu
+    {
+        static internal void Show()
+        {
+            Console.WriteLine("Welcome To the Option Menu");
+            Console.WriteLine("Press 1: Create Fueltank");
+            Console.WriteLine("Press 2: Modify or Create PetrolPump");
+            Console.WriteLine("Press 3: Create PayStation");
+            string input = Console.ReadLine();
+            int selection;
+            bool parsingSuccessful = Int32.TryParse(input, out selection);
+            if (parsingSuccessful)
+            {
+                switch (selection)
+                {
+                    case 1:
+                        Console.WriteLine("Do Something");
+                        break;
+                    case 2:
+                        ModifyOrCreatePump();
+                        break;
+                    case 3:
+                        Console.WriteLine("Do Something other");
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("!!!!!!!!!!!!!!!!!!! INVALID INPUT !!!!!!!!!!!!!!!!!");
+            }
+        }
+
+        static internal void ModifyOrCreatePump()
+        {
+            Console.Write("Select a PetrolPump or create one with 'create'\nThe Pumps: {");
+            foreach (int id in PetrolPumpController.GetInstance().GetListOfPumpIds())
+            {
+                Console.Write(String.Format("{0} ", id));
+            }
+
+            Console.Write(" }\n");
+            string input = Console.ReadLine();
+            if (input.ToLower().Contains("create"))
+            {
+                // the pump add calls
+                new PetrolPump();
+                Console.Write("Created one PetrolPump\n");
+            }
+            else
+            {
+                int pumpId;
+                Int32.TryParse(input, out pumpId);
+                PetrolPump pump = PetrolPumpController.GetInstance().GetPump(pumpId);
+                Console.Write(pump.GetAmountToPay());
+            }
+        }
+    }
+}
--- PetrolStation/Petrolstation/Petrolstation.UserInterface/Program.cs
+++ PetrolStation/Petrolstation/Petrolstation.UserInterface/Program.cs
@@ -13,14 +13,13 @@ namespace Petrolstation.UserInterface
         {
             while (true)
             {
-                Console.WriteLine("Welcome To the Option Menu");
-                Console.WriteLine("Press 1: Create Fueltank");
-                Console.WriteLine("Press 2: Modify or Create PetrolPump");
-                Console.WriteLine("Press 3: Create PayStation");
+                Console.WriteLine("Welcome to the main menu of the petrol station");
+                Console.WriteLine("Press 1: Enter the Petrol Station Simulation software");
+                Console.WriteLine("Press 2: To add Petrol Station Parts to the Simulation software");
                 string input = Console.ReadLine();
                 int selection;
-                bool parsingSuccessful = Int32.TryParse(input, out selection);
-                if (parsingSuccessful)
+                bool conversionSuccessful = Int32.TryParse(input, out selection);
+                if (conversionSuccessful)
                 {
                     switch (selection)
                     {
@@ -28,47 +27,16 @@ namespace Petrolstation.UserInterface
                             Console.WriteLine("Do Something");
                             break;
                         case 2:
-                            SelectOrCreatePump();
-                            break;
-                        case 3:
-                            Console.WriteLine("Do Something other");
+                            OptionMenu.Show();
                             break;
                         default:
                             break;
                     }
                 }
-                else
-                {
-                    Console.WriteLine("!!!!!!!!!!!!!!!!!!! INVALID INPUT !!!!!!!!!!!!!!!!!");
-                }
-                
             }
         }
 
-        static private void SelectOrCreatePump()
-        {
-            Console.Write("Select a PetrolPump or create one with 'create'\nThe Pumps: {");
-            foreach (int id in PetrolPumpController.GetInstance().GetListOfPumpIds())
-            {
-                Console.Write(String.Format("{0} ", id));
-            }
-
-            Console.Write(" }\n");
-            string input = Console.ReadLine();
-            if (input.ToLower().Contains("create"))
-            {
-                // the pump add calls
-                new PetrolPump();
-                Console.Write("Created one PetrolPump\n");
-            }
-            else
-            {
-                int pumpId;
-                Int32.TryParse(input, out pumpId);
-                PetrolPump pump = PetrolPumpController.GetInstance().GetPump(pumpId);
-                Console.Write(pump.GetAmountToPay());
-            }
-        }
+        
         static private void SelectTap()
         {
 
