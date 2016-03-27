9f5fd532da3344d9b5e02d30d67bcbdc535a3a9d
Reto Bürgisser <buergissera@hotmail.com>
2016-03-27 16:18:01 +0100

    Add Method splitting in UI

--- PetrolStation/Petrolstation/Petrolstation.UserInterface/Program.cs
+++ PetrolStation/Petrolstation/Petrolstation.UserInterface/Program.cs
@@ -11,29 +11,35 @@ namespace Petrolstation.UserInterface
     {
         static void Main(string[] args)
         {
-            while(true)
+            PetrolPumpController pumpController = PetrolPumpController.GetInstance();
+            PayStationController payStationConroller = PayStationController.GetInstance();
+
+            while (true)
             {
-                PetrolPumpController pumpController = PetrolPumpController.GetInstance();
-                PayStationController payStationConroller = PayStationController.GetInstance();
+                SelectOrCreatePump();
+            }
+        }
 
-                Console.Write("Select a PetrolPump or create one with 'create'\nThe Pumps: {");
-                foreach (int id in pumpController.GetListOfPumpIds())
-                {
-                    Console.Write(String.Format("{0} ", id));
-                }
-                Console.Write(" }\n");
-                string input = Console.ReadLine();
-                if (input.ToLower().Contains("create"))
-                {
-                    // the pump add calls
-                    new PetrolPump();
-                    Console.Write("Created one PetrolPump\n");
+        static private void SelectOrCreatePump()
+        {
+            Console.Write("Select a PetrolPump or create one with 'create'\nThe Pumps: {");
+            foreach (int id in PetrolPumpController.GetInstance().GetListOfPumpIds())
+            {
+                Console.Write(String.Format("{0} ", id));
+            }
 
-                }
-                else
-                {
-
-                }
+            Console.Write(" }\n");
+            string input = Console.ReadLine();
+            if (input.ToLower().Contains("create"))
+            {
+                // the pump add calls
+                new PetrolPump();
+                Console.Write("Created one PetrolPump\n");
+            }
+            else
+            {
+                int pumpId;
+                Int32.TryParse(input, out pumpId);
             }
         }
     }
