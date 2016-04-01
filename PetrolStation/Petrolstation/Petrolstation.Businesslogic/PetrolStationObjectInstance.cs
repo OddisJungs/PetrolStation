0a37b79570275b0986b9f0e95d688d62bc04fe34
Reto Bürgisser <buergissera@hotmail.com>
2016-04-01 22:15:56 +0100

    Fix Bug not Serializable PetrolStationInstanceObject

--- PetrolStation/Petrolstation/Petrolstation.Businesslogic/PetrolStationObjectInstance.cs
+++ PetrolStation/Petrolstation/Petrolstation.Businesslogic/PetrolStationObjectInstance.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Petrolstation.Businesslogic
 {
+    [Serializable]
     public class PetrolStationObjectInstance : DataItem
     {
         // Private Members
