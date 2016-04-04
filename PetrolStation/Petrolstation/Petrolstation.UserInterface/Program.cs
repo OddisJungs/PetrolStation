a1e04ccc621cb7ad74395c8df80d2678b24ad7e9
C3D1 <cedric_schnider@outlook.com>
2016-04-04 21:30:53 +0200

    Added the first part of the QuittanceManagement

--- PetrolStation/Petrolstation/Petrolstation.UserInterface/Program.cs
+++ PetrolStation/Petrolstation/Petrolstation.UserInterface/Program.cs
@@ -16,6 +16,7 @@ namespace Petrolstation.UserInterface
                 Console.WriteLine("Welcome to the main menu of the petrol station");
                 Console.WriteLine("Press 1: Enter the Petrol Station Simulation software");
                 Console.WriteLine("Press 2: To add Petrol Station Parts to the Simulation software");
+                Console.WriteLine("Press 3 : Enter QuittanceManagement for more information about the fuelling progress");
                 string input = Console.ReadLine();
                 int selection;
                 bool conversionSuccessful = Int32.TryParse(input, out selection);
@@ -28,6 +29,9 @@ namespace Petrolstation.UserInterface
                             break;
                         case 2:
                             OptionMenu.Show();
+                            break;
+                        case 3:
+                            QuittanceManagement.Show();
                             break;
                         default:
                             break;
---
+++ PetrolStation/Petrolstation/Petrolstation.UserInterface/QuittanceManagement.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Petrolstation.UserInterface
+{
+    internal class QuittanceManagement
+    {
+        public static void Show()
+        {
+            Console.WriteLine("Welcome to the QuittanceManagement");
+            Console.WriteLine("Press 1 : To see the earnings");
+            Console.WriteLine("Press 2 : To see the liters tanked per fueltype");
+            string input = Console.ReadLine();
+            if (!string.IsNullOrEmpty(input))
+            {
+                int intInput;
+                bool parsingsucceded = Int32.TryParse(input, out intInput);
+                if (parsingsucceded)
+                {
+                    if (intInput == 1)
+                    {
+                        ListAllEarnings();
+                    }
+                    else if (intInput == 2)
+                    {
+                        LitersTankedPerFueltype();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid Input, back to the main menu");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input, back to the main menu");
+            }
+        }
+
+        static internal void ListAllEarnings()
+        {
+            
+        }
+
+        static internal void LitersTankedPerFueltype()
+        {
+            
+        }
+    }
+}
